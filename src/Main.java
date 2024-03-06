c939b0203ec1c5cbcd353b8c24f97e39deba6977
Luiz <Luiz Eduardo Alves Constancio>
2024-03-06 16:39:52 -0300

    Add piece and board class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
+import boardGame.Board;
 import boardGame.Position;
 
 public class Main {
     public static void main(String[] args) {
-        Position position = new Position(2,3);
-        System.out.print(position);
+        Board board = new Board(8,8);
     }
 }
---
+++ src/boardGame/Piece.java
@@ -0,0 +1,16 @@
+package boardGame;
+
+public class Piece {
+    protected Position position;
+    private Board board;
+
+    public Piece(Board boardgame) {
+        this.board = boardgame;
+        position = null;
+    }
+
+    protected  Board getBoard(){
+        return  board;
+    }
+
+}
