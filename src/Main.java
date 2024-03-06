4b048fea77b1e019d96721a0bfefc3c9808331ce
Luiz <Luiz Eduardo Alves Constancio>
2024-03-06 16:28:00 -0300

    Add position class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import boardGame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Position position = new Position(2,3);
+        System.out.print(position);
     }
 }
