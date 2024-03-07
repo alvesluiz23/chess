6d6562f11ade4d4d0663706b16d71746ca9540c7
Luiz <Luiz Eduardo Alves Constancio>
2024-03-07 15:53:54 -0300

    Add position convert logic

---
+++ src/chess/ChessException.java
@@ -0,0 +1,9 @@
+package chess;
+
+public class ChessException extends RuntimeException {
+    public static long setialVersionUID = 1l;
+
+    public ChessException(String msg){
+        super(msg);
+    }
+}
